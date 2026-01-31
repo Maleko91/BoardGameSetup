8bd4681a32ab50fdc2547c3e96706dde3822915e
Maleko91 <iamyesterdayshero@gmail.com>
2026-01-31 11:13:26 +0000

    Fixing tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,21 @@
 import "@testing-library/jest-dom/vitest";
 
+if (!window.matchMedia) {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: (query: string) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => false
+    })
+  });
+}
+
 vi.mock("./lib/supabase", () => ({
   get supabaseReady() {
     return Boolean((globalThis as { __supabaseMockState?: { ready: boolean } })
